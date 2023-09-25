83159c7fc6bd90810b29316decefc49540e3d956
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2023-09-25 19:47:27 +0530

    ALLOWED_HOSTS configured

--- vehicularHonk/settings.py
+++ vehicularHonk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ah5rue^(jkikvf=*)#q$(*_#%nnn+f(coqcdhm7g&a54^1*)%-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
