8c5cf9a14cf5cd92993167bd9e2dfcf4d4dc5099
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2023-09-25 17:19:22 +0530

    Django REST setup

---
+++ vehicularHonkAPI/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    # path('modelTest',views.modelTest,name='modelTest'),
+    # path('gpt2Test',views.gpt2Test,name='gpt2Test'),
+]
---
+++ vehicularHonkAPI/views.py
@@ -0,0 +1,70 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import tensorflow as tf
+import os
+from vehicularHonk.settings import BASE_DIR
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense,Dropout,BatchNormalization
+from tensorflow.keras.models import Model
+# Create your views here.
+
+def createModel():
+    image_size = 224  # We'll resize input images to this size
+    num_patches = 8
+    patch_size =224//num_patches   # Size of the patches to be extract from the input images
+    input_shape=(224,224,3)
+    # Input dimensions
+    input_shape = (224, 224, 3)
+    # Patch size
+    # Number of classes
+    num_classes = 4
+
+    # Input layer
+    inputs = Input(shape=input_shape)
+
+    # Divide the image into patches
+    patches = tf.image.extract_patches(images=inputs,
+                                sizes=[1, image_size//patch_size, image_size, 1],
+                                strides=[1,image_size//patch_size, image_size, 1],
+                                rates=[1, 1, 1, 1],
+                                padding='VALID')
+
+    # Reshape the patches to have 3 channels
+    patches = tf.reshape(patches, [-1, patch_size, image_size, 3*(image_size//patch_size)])
+    print(patches.shape)
+
+    # CNN for patch processing
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(patches)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    flat = Flatten()(pool2)
+    fc1 = Dense(128, activation='relu')(flat)
+    drop=Dropout(0.5)(fc1)
+    outputs = Dense(num_classes,activation="softmax")(drop)
+
+
+    # Define the model
+    model = Model(inputs=inputs, outputs=outputs)
+    print(model.summary())
+
+    # Compile the model
+    model.compile(
+        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=3e-4),
+        metrics=["accuracy"]
+    )
+    model.load_weights(os.path.join(BASE_DIR,f"vehicularHonkAPI\patchedModels\patched{num_patches}weights.h5"))
+    return model
+
+model=createModel()
+
+@api_view(['POST'])
+def modelTest(request):
+    return JsonResponse({"state":"Success"})
+
+def predictClass():
+    audioClass=0
+    
+    return audioClass
